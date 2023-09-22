25158d1492103d07f3883ca9a8cf5b9788940203
Sirakusa <herrera.clip@gmail.com>
2023-09-21 18:55:11 -0600

    defined methods

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,24 @@
 
 class CommentRepository{
-    
+
+    constructor(){
+        this.Comment = []
+        this.id = 0
+    }
+
+    create(){
+
+    }
+
+    delete(){
+
+    }
+
+    uptdate(){
+
+    }
+
+    read(){
+        
+    }
 }
