3c64ec693e7f44a2549f6addc326b6c1d24c7d7b
Sirakusa <herrera.clip@gmail.com>
2023-09-23 12:58:06 -0600

    defined post routes

--- router.js
+++ router.js
@@ -1,8 +1,42 @@
 
+import {PostRepository} from "./models/Post.js"
+import {CommentRepository} from "./models/Comment.js"
+
 const expres = require('express')
 
 const app = expres()
 const port = 3000
+
+const post_obj = new PostRepository()
+const coment_obj = new CommentRepository()
+
+app.use(bodyParser.json())
+
+
+app.post('post/',(req,res) =>{
+    const postData = req.body
+
+
+})
+
+
+app.get('post/get/:id',(req,res) =>{
+    const id = req.params.idPost
+
+    
+})
+
+
+app.delete('post/delite/:id',(req,res) => {
+    const delite = req.params.id
+
+
+})
+
+app.patch('post/patch/:id/:description', (req,res) => {
+
+})
+
 
 
 
